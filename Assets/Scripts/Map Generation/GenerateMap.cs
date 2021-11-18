ef8707b2408fdd1d20f73ac325b7126814e79bd5
CollinTift <collin.tift@gmail.com>
2021-11-18 04:55:00 -0500

    Room generation

--- Assets/Scripts/Map Generation/GenerateMap.cs
+++ Assets/Scripts/Map Generation/GenerateMap.cs
@@ -2,16 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Linq;
 
 public class GenerateMap : MonoBehaviour {
     Tilemap tilemap;
 
     [Header("Base Sprite")]
     public Sprite baseSprite;
+    public Sprite testSprite;
 
     [Header("Map attributes")]
     public int sizeX;
     public int sizeY;
+    public int numRooms;
+    public int minRoomDimension;
+    public int maxRoomDimension;
+    private Room[] rooms;
 
     [Header("Tile Sprites")]
     public Sprite[] floorSprites;
@@ -19,12 +25,64 @@ public class GenerateMap : MonoBehaviour {
 
     private void Awake() {
         tilemap = GetComponent<Tilemap>();
+        rooms = new Room[numRooms];
 
         ApplyBase();
 
         floorTiles = InstantiateTiles(floorSprites);
 
-        Room room = new Room();
+        GenerateRooms();
+        GenerateCorridors();
+    }
+
+    private void GenerateRooms() {
+        //generate room and test to make sure it does not collide with others, if it does generate new until max is rteached
+        int index = 0;
+        while (index < numRooms) {
+            Vector2Int randPos = new Vector2Int(Random.Range(0, sizeX), Random.Range(0, sizeY));
+            int randWidth = Random.Range(minRoomDimension, maxRoomDimension);
+            int randHeight = Random.Range(minRoomDimension, maxRoomDimension);
+            Room.RoomType randType = (Room.RoomType)Random.Range(0, (int)Room.RoomType.COUNT);
+            int randNumDoors = Random.Range(1, 3) * 2;
+
+            Room newRoom = new Room(randPos, randWidth, randHeight, randType, randNumDoors);
+
+            bool safeToUse = true;
+
+            if (newRoom.pos.x + newRoom.width <= sizeX && newRoom.pos.y + newRoom.height <= sizeY && newRoom.pos.x >= 0 && newRoom.pos.y >= 0) {
+                for (int i = 0; i < index; i++) {
+                    if (newRoom.CollidesWith(rooms[i])) {
+                        safeToUse = false;
+                        break;
+                    }
+                }
+            } else {
+                safeToUse = false;
+            }
+
+            if (safeToUse) {
+                rooms[index] = newRoom;
+                index++;
+            }
+        }
+
+        //draw tiles for each room on tilemap
+        Tile testTile = ScriptableObject.CreateInstance<Tile>();
+        testTile.sprite = testSprite;
+        testTile.color = Color.white;
+
+        for (int i = 0; i < index; i++) {
+            for (int x = rooms[i].pos.x; x < rooms[i].pos.x + rooms[i].width; x++) {
+                for (int y = rooms[i].pos.y; y < rooms[i].pos.y + rooms[i].height; y++) {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), testTile);
+                }
+            }
+        }
+    }
+
+    //simple Z shaped corridor generation between doors
+    private void GenerateCorridors() {
+        
     }
 
     private void ApplyBase() {
--- Assets/Scripts/Map Generation/Room.cs
+++ Assets/Scripts/Map Generation/Room.cs
@@ -3,25 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Room {
-    int x, y;
-    int width, height;
+    public Vector2Int pos;
+    public int width, height;
+    public int numDoors;
+    public Door[] doors;
 
     public enum RoomType {
         combat,
         loot,
         trap,
-        exit
+        entrance,
+        exit,
+        COUNT
     }
 
     RoomType roomType;
 
-    public Room(int x, int y, int width, int height, RoomType roomType) {
-        this.x = x;
-        this.y = y;
+    public Room(Vector2Int pos, int width, int height, RoomType roomType, int numDoors) {
+        this.pos = pos;
 
         this.width = width;
         this.height = height;
 
         this.roomType = roomType;
+
+        this.numDoors = numDoors;
+
+        GenerateDoors();
+    }
+
+    public bool CollidesWith(Room room) {
+        for (int x = pos.x; x < pos.x + width; x++) {
+            for (int y = pos.y; y < pos.y + height; y++) {
+                // for each tile in this room, see if it is within the bounds of the parameter room
+                if (x >= room.pos.x && x < room.pos.x + room.width) {
+                    if (y >= room.pos.y && y < room.pos.y + room.height) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void GenerateDoors() {
+        
+    }
+
+    public class Door {
+        public Vector2Int pos;
+        public Door connectedDoor;
+
+        public Door(int x, int y) {
+            pos = new Vector2Int(x, y);
+            connectedDoor = null;
+        }
+
+        public void ConnectTo(Door target) {
+            connectedDoor = target;
+        }
     }
 }
